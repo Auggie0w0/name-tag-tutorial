b0fc2b5d611329e4d14513b8a895cd365e9d2f0e
August <117049354+Auggie0w0@users.noreply.github.com>
2024-04-23 16:01:29 -0700

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,3 @@
+basic.show_string("My name's August and I am")
+basic.show_number(16)
+basic.show_string("years old!")
---
+++ main.ts
@@ -0,0 +1,3 @@
+basic.showString("My name's August and I am")
+basic.showNumber(16)
+basic.showString("years old!")
